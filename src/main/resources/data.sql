8a54df5d0f9b047bc4c9c1ddc4efdb0504e3e4c6
anoguez-ias <anoguez@itexico.com>
2018-12-02 23:42:10 -0600

    Add data to prepopulate the employee table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
-insert into employee
-values(1,'Jose','Alfonso','Noguez Flores','1984-06-28 12:00:00','2019-01-01 12:00:00','1');
+insert into employee (id, first_name, middle_initial, last_name, date_of_birth, date_of_employment, status)
+values(1,'Jose Alfonso','A.','Noguez Flores','1984-06-28','2019-01-01', 1),
+(2,'Pedro','','Martinez Vazquez','1984-04-21','2019-01-01', 1),
+(3,'Jose','','Ceballos Flores','1989-11-28','2019-01-01', 1),
+(4,'Estela','','Ceballos Sanchez','1992-12-06','2019-01-01', 1),
+(5,'Jose Francisco','F.','Gonzalez Flores','1980-02-13','2019-01-01', 1);
